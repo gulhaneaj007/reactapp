56f6702cf7b8072cea099643f4cccaca7b9beda4
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-05-31 03:33:37 +0530

    Handle array list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import './App.css';
+import React from "react";
+import { Table } from "react-bootstrap";
+// import { Alert, Button,Spinner } from 'react-bootstrap';
+import "./App.css";
 function App() {
+  const student = [
+    { name: "ajinkya", email: "ajinkya@test.com", no: 7884546 },
+    { name: "rahul", email: "rahul@test.com", no: 5656556 },
+    { name: "vinayak", email: "vinayak@test.com", no: 1338788 },
+    { name: "Suhas", email: "Suhas@test.com", no: 5555688 },
+  ];
+ 
+  student.map((key) => {
+    console.log("my name is :", key);
+    return true;
+  });
+
   return (
-    <div className="App">
-       <h1>hello</h1>
-        <Button>Click Me</Button>
-    </div>
+    <>
+     
+      <div className="App container">
+        <h1>Handle array with list</h1>
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Sr no</th>
+              <th> Name</th>
+              <th>Email</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+            {student.map((data,ind) => (
+              <>
+                <tr>
+                <td>{ind}</td>
+                  <td>{data.name}</td>
+                  <td>{data.email}</td>
+                  <td>{data.no}</td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
 
