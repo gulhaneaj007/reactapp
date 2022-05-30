2a0d730a1318f55fdeb3676f4c61012896317b93
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-05-31 04:33:10 +0530

    Nested List In React JS

--- src/App.js
+++ src/App.js
@@ -1,54 +1,71 @@
 import React from "react";
 import { Table } from "react-bootstrap";
-// import { Carousel } from 'react-bootstrap';
 import "./App.css";
 function App() {
   const result = [
-    { name: "Ajinkya", email: "ajinkya@test.com.", no: 7884546 },
-    { name: "Vindayak", email: "rahul@test.com.", no: 5656556 },
-    { name: "Rahul", email: "vinayak@test.co.", no: 1338788 },
-    { name: "Arvind", email: "Suhas@test.com.", no: 5555688 },
+    { name: "Ajinkya", email: "ajinkya@test.com.", no: 7884546,Address :
+      [{HN:'10',city:"pune",country :"India"},
+        {HN:'20',city:"noida",country :"India"},
+        {HN:'40',city:"mumbai",country :"India"}
+      ] 
+    },
+    { name: "Vindayak", email: "rahul@test.com.", no: 5656556,Address :
+      [{HN:'50',city:"delhi",country :"India"},
+      {HN:'40',city:"mumbai",country :"India"},
+      {HN:'20',city:"noida",country :"India"}
+      ] 
+    },
+    { name: "Rahul", email: "vinayak@test.co.", no: 1338788,Address :
+      [{HN:'50',city:"delhi",country :"India"},
+      {HN:'40',city:"mumbai",country :"India"},
+      {HN:'10',city:"pune",country :"India"}
+      ]
+    },
+    { name: "Arvind", email: "Suhas@test.com.", no: 5555688,Address :
+      [{HN:'40',city:"mumbai",country :"India"},
+        {HN:'20',city:"noida",country :"India"}
+      ]
+    }
   ];
-
-  // result.map((key) => {
-  //   console.log("my name is :", key);
-  //   return true;
-  // });
-
   return (
     <>
-      <div className="App container">
+      <div className="App ">
         <h1>Handle array with list</h1>
-        {/* <Carousel>
-          {result.map((key)=>(
-            <Carousel.Item>
-              <img className="d-block w-100" src="https://images.ctfassets.net/hrltx12pl8hq/4N1NeoHTaT8nI0x7p1jCGk/5a6054a5046b7749a6b78ad3ca1eb57f/water-splash-clr-shutterstock_258421805.jpg?fit=fill&w=480&h=270&fm=webp" alt="First slide" />
-              <Carousel.Caption>
-                <h3>{key.name} : slide lable</h3>
-                <p>{key.email}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-          ))}
-          
-        </Carousel> */}
-
-        <Table striped bordered variant="dark" size="sm">
+        <Table striped variant="dark" >
           <thead>
             <tr>
+              
               <th>Sr No</th>
+              <th>Address</th>
               <th> Name</th>
               <th>Email</th>
               <th>Contact</th>
             </tr>
           </thead>
           <tbody>
-            {result.map((data, i) => (
+            {result.map((item, i) => (
               <tr key={i}>
-                <td>{i}</td>
-                <td>{data.name}</td>
-                <td>{data.email}</td>
-                <td>{data.no}</td>
+                <td>{i+1}</td>
+                <td>
+                  <Table striped variant="dark" >
+                    <tbody>
+                      {
+                        item.Address.map((arr_data,j)=>
+                        <tr key ={j}>
+                          <td>{arr_data.HN}</td>
+                          <td>{arr_data.city}</td>
+                          <td>{arr_data.country}</td>
+                        </tr>
+                        )
+                      }
+                    </tbody>
+                  </Table>
+                </td>
+                
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.no}</td>
+                
               </tr>
             ))}
           </tbody>
