68d9bafe910f8a928015805205c810b9ce173ed6
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-05-31 02:30:40 +0530

    start kit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import "./App.css";
-<<<<<<< HEAD
- 
-
-import {Button} from "react-bootstrap"
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './App.css';
 function App() {
   return (
-    <>
-      <div className="App ">
-
-        <Button className="primary">Click me</Button>
-
-=======
-import React from "react";
-import ShouldComponentUpdate from "./shouldComponentUpdate/ShouldComponentUpdate";
-function App() { 
-
-
-  return (
-    <> 
-      <div className="App">
-        <h1>App Component hi</h1>
-        <ShouldComponentUpdate />  
->>>>>>> 56fd16a95c6f75320be911d4feddce6c514ce2bf
-      </div>
-    </>
+    <div className="App">
+       <h1>hello</h1>
+        <Button>Click Me</Button>
+    </div>
   );
 }
 
