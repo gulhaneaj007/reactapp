7baeb948bae325cc35a7d47ce52a75b5bbdfdae0
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-06-02 01:18:59 +0530

    useMemo Hooks in React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import "./App.css";
-import User from "./User";
-// class App extends React.PureComponent {  //or you can use like this
-class App extends React.Component {
+function App() {
+  const [count, setcount] = useState(0);
+  const [item, setitem] = useState(10);
 
-  constructor() {
-    super();
-    this.state = {
-      count: 1,
-    };
-  }
 
-  render() {
-    return (
-      <div className="App container"><br></br>
-        <User count={this.state.count}/>
-        <button onClick={()=>this.setState({count:1})}>Update Count</button>
-      </div>
-    );
-  }
+  const multcontMemo=useMemo(  function multicount()
+  {
+    console.warn("multicount")
+    return count*5
+  },[count])
+
+  return (
+    <div className="App container">
+      <br></br>
+      <h1>useMemo Hooks in React JS</h1>
+      <h2>Count : {count}</h2>
+      <h2>Count : {item}</h2>
+      <h2>multicount : {multcontMemo}</h2>
+      <button onClick={() => setcount(count + 1)}>Update count</button>
+      <br />
+      <br />
+      <button onClick={() => setitem(item * 10)}>Update item</button>
+    </div>
+  );
 }
 
 export default App;
-//check console
-//
