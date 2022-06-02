1c950362deb0c882fc9fcd5f58fb403ec3435ea8
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-06-02 05:43:42 +0530

    forwordRef Hooks in React JS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, {  useRef } from "react";
+import React,{useRef} from "react";
+import Child from "./Child";
 import "./App.css";
 function App() {
-  var inputRef=useRef(null)
-  function handleinput(){
-    console.warn("function called")
-    inputRef.current.value="100"
+
+  function updateInput(){
+    inputRef.current.value="1000"
     inputRef.current.style.color="red"
     inputRef.current.style.backgroundColor="Yellow"
-    // inputRef.current.style.display="none"
-
-
-
     inputRef.current.focus()
 
+
+
   }
+
+  let inputRef=useRef(null)
   return (
     <div className="App container">
-      <h1 className="display-2">useRef Hooks in React JS</h1>
-      <input type="text" ref={inputRef} />
-      <br />
-      <button onClick={handleinput} className="btn btn-sm btn-success my-2">
-        Click Me
-      </button>
-      
+      <h1>forwordRef Hooks in React JS</h1>
+      <Child ref={inputRef}/>
+      <br/>
+      <br/>
+      <button onClick={updateInput} className="btn btn-sm btn-success">Update Input</button>
     </div>
   );
 }
---
+++ src/Child.js
@@ -0,0 +1,10 @@
+import React,{forwardRef} from "react";
+ function Child(props,ref) {
+  return (
+    <>
+      <input type="text"  ref={ref}/>
+    </>
+  );
+}
+
+export default forwardRef(Child)
